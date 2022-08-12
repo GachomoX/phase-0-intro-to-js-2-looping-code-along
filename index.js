950c36289d997623fef69fa0bea2f89b10eed6b9
Dennis <dennisgachomo@gmail.com>
2022-08-12 19:53:13 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names, event) {
+  let thanks =[];
+  event = "surprise";
+  for (let i= 0; i < names.length; i++) {
+  thanks[i] = 'Thank you, '+ names[i] +', for the wonderful ' +event+' gift!';
+  }
+  return thanks;
+}
+
+
+let number = 10;
+function countDown (number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+} 
